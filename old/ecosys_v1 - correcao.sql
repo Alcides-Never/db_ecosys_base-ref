a60820ae3a469386f041482b9c0356a72a46a41f
Alcides Paz <alcidesnp@hotmail.com>
2023-11-20 23:02:16 -0300

    Atualização script

---
+++ old/ecosys_v1 - correcao.sql
@@ -0,0 +1,12 @@
+USE db_ecosystem;
+
+ALTER TABLE endereco ADD DataCriacao datetime;
+ALTER TABLE endereco ADD DataModificacao datetime;
+ALTER TABLE usuario ADD DataCriacao datetime;
+ALTER TABLE usuario ADD DataModificacao datetime;
+ALTER TABLE anuncio ADD DataCriacao datetime;
+ALTER TABLE anuncio ADD DataModificacao datetime;
+ALTER TABLE coleta ADD DataCriacao datetime;
+ALTER TABLE coleta ADD DataModificacao datetime;
+ALTER TABLE produto ADD DataCriacao datetime;
+ALTER TABLE produto ADD DataModificacao datetime;
